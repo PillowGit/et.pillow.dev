970f24816d70a010362f6b831ae18366b4660f42
PillowGit <goldentaco727@gmail.com>
2025-01-24 13:17:39 -0800

    Change site title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-	import { onMount } from 'svelte';
-	import { animate } from 'motion';
-	import appointments from '$lib/resources/appointments.json';
+	import { onMount } from "svelte";
+	import { animate } from "motion";
+	import appointments from "$lib/resources/appointments.json";
 
 	// Time conversion functions
 	// All times are stored as "epoch minutes from January 19th, 2025, 00:00",
 	// which represents the start of the semester.
 	function toEpoch(date) {
-		const diff = date.getTime() - new Date('2025-01-19T00:00:00Z').getTime();
+		const diff = date.getTime() - new Date("2025-01-19T00:00:00Z").getTime();
 		return Math.floor(diff / 60000);
 	}
 	function toDate(epoch) {
-		return new Date(new Date('2025-01-19T00:00:00Z').getTime() + epoch * 60000);
+		return new Date(new Date("2025-01-19T00:00:00Z").getTime() + epoch * 60000);
 	}
 
 	onMount(() => {
@@ -20,6 +20,7 @@
 	});
 </script>
 
+<title>ET Schedule</title>
 <!-- Colors from https://www.pinterest.com/pin/323133342032115696/ -->
 <div
 	class="flex h-screen w-screen flex-col items-center justify-center bg-[#0A0A0A] text-[#F5F5F5]"
